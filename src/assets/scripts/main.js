561355416b42f77e16ea50c3de4ce5febc2bca8b
Guillermo Ruiz <grugu@uoc.edu>
2025-03-16 13:31:00 +0100

    adding slider

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,8 +3,10 @@
  * see commented examples below
  */
 
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
+import $ from 'jquery';
+import 'slick-carousel';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 
 /**
  * Write any other JavaScript below
@@ -13,4 +15,18 @@
 +( function() {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
+
+  /**
+   * Slick carousel configuration
+   */
+  $('.my-slider').slick({
+    slidesToShow: 1,  
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false
+  });
+  
 } )();
+
+
+
